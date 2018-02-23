591c7ac859349ee512f622521116260feab7ab3f
psoerens <p.soerens@outlook.com>
2018-02-22 16:12:02 -0800

    class form of the LargeNumberReader

--- LargeNumberReader.py
+++ LargeNumberReader.py
@@ -1,41 +1,42 @@
 class LargeNumberReader():
 	'''A class for reading in large numbers into python'''
 
-	largeNum = []	#instantiation of the new list representing the large number, set as integers
-	
 	def __init__(self, file):
-
-		self.largeNumberTxt = file
-		i = 0	#line of the file
-		d = 0	#number of digits read in
-
+		'''Initialization via pasing in the text file to be read'''
+		self.largeNumberTxt = file	#set the textfile attribute
 		
-		l = []			#temporary list
-
-		#rows, columns, number of digits in number
-		r = 0
-		c = 0
-		n = 0	
-
+	def getLarge(self):
+		'''get the large number, parse through the file'''
+		'''The expected format is:
+			line 0: rows, columns, number of digits
+			line [1:r]: the digits on lines not longer than columns
+		'''
+		
 		#largeNumberTxt = '1000digit.txt'	#The file to be read from
 	
-	with open(largeNumberTxt) as file_object:	#open the object
-		lines = file_object.readlines()			#read lines into a list
+		with open(self.largeNumberTxt) as file_object:	#open the object
+			lines = file_object.readlines()			#read lines into a list
+			
+		i = 0	#line of the file
+		d = 0	#number of digits read in
+		largeNum = []	#instantiation of the new list representing the large number, set as integers
 		
-	def getReadNumber(self):
-																							if (i == 0):	#comma separate variables of line "0"
-		print(lines[i].rstrip())
-		dims = lines[i].rstrip().split(",")
-		r = int(dims[0])
-		c = int(dims[1])
-		n = int(dims[2])
-		#print("rows: " + str(r) + ", cols:"+ str(c) + ", dig:" + str(n))
-		i+=1	#index for the next line stored in lines
+		if  i == 0:	#comma separate variables of line "0"
 
+			#print(lines[i].rstrip())
+			dims = lines[i].rstrip().split(",")
+			r = int(dims[0])
+			c = int(dims[1])
+			n = int(dims[2])
+			print("rows: " + str(r) + ", cols:"+ str(c) + ", dig:" + str(n))
+			i+=1	#index for the next line stored in lines
+
+		
 		while d < n: #begin looping through the digit of the number
 			l = lines[i].rstrip()
-			#print(l)
-			#print(len(l))
+			#print("this is l " + l)
+			#print("this is its length " + str(len(l)))
+			#print("this is number of digits total " + str(n))
 			strLen = len(l)
 			
 			if strLen > c:	#formatting error
@@ -47,12 +48,8 @@ class LargeNumberReader():
 			
 			d+=strLen	#iterate through max length of line
 			i+=1		#iterate to next line
+			
+		return largeNum
 
 
 
-
-	
-
-
-#print(i)
-#print(largeNum)
