0d0a5bf206b29ea73dadfeba256c68310c5ffd52
psoerens <p.soerens@outlook.com>
2018-02-26 14:39:59 -0800

    update to include lnr

--- Euler8.py
+++ Euler8.py
@@ -1,20 +1,34 @@
-twenty = [1,2,3,4,5,6,7,8,9,0,1,3,5,7,9,2,4,6,8,0] #testing list
+from LargeNumberReader import LargeNumberReader #useing the large number reader
+
+
+lnr = LargeNumberReader('1000digit.txt')	#initialize the reader
+large = lnr.getLarge()						#get the large number in a list format
+
 
 index = 0	#initial index
-lens = 4 #length of lens
+lens = 13 #length of lens
 
-length = len(twenty)
-last = length - lens+1 #last index to check
-#print(last)
+length = len(large)	#length of the list
+last = length - lens+1 #one beyond the last index to check
+#print("The ending index to exit: " + str(last))
+
 largeMulti = 0 #The largest product so far
-
+indexFound = [0] * lens #range where found
+#print(indexFound)
 for i in range(index, last) :
 	
-	temp = (twenty[i])*(twenty[i+1])*(twenty[i+2])*(twenty[i+3])
-	print(temp)
+	temp = large[i]	#store the fist value
+	for j in range(1,lens) :
+		temp *= large[i+j] #get product with remaining values in lens range
+	#print(temp)
 	
 	if temp > largeMulti:
 		largeMulti = temp
+		
+		for j in range(0, lens):	#largest product indexes
+			indexFound[j] = i + j
+			
 	
-print(i)
-print(largeMulti)
+#print("index gotten to at exit: " + str(i))
+print("larges value found: " + str(largeMulti))
+#print("found at: " + str(indexFound))
